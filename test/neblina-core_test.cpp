c1feac0d33f7dbc2d6fb7e4004bf8c1471fa6f57
paulo <paulo@localhost>
2021-03-26 00:20:34 -0300

    updated the test to have a class to add setup and teardown methods
    added a method to test addVectorFC
    added a method to test subVector
    
    updated the tests to use expect with correct values (tests are real now)
    
    updated the tests to use vector_new for the result vector as well

--- test/neblina-core_test.cpp
+++ test/neblina-core_test.cpp
@@ -6,68 +6,142 @@
 
 using namespace std;
 
-TEST(neblina_test_suite, showDeviceList)
-{
-    showDevicesList();
-    
-    EXPECT_EQ(1, 1);
-    
-}
-
-TEST(neblina_test_suite, addVector)
-{
-    
+class NeblinaCoreFixture : public ::testing::Test {
+protected:
     cl_int err;
     cl_uint num_platforms;
+    cl_int status;
+public:
 
-    err = clGetPlatformIDs(0, NULL, &num_platforms);
-    if (err == CL_SUCCESS) {
-        std::cout << "Success. Platforms available: " << num_platforms
-                << std::endl;
-    } else {
-        std::cout << "Error. Platforms available: " << num_platforms
-                << std::endl;
+    NeblinaCoreFixture() {
+        // initialization code here
     }
 
-    InitCLEngine();
+    void SetUp() {
+        err = clGetPlatformIDs(0, NULL, &num_platforms);
+        if (err == CL_SUCCESS) {
+            std::cout << "Success. Platforms available: " << num_platforms
+                    << std::endl;
+        } else {
+            std::cout << "Error. Platforms available: " << num_platforms
+                    << std::endl;
+        }
 
-    vector_t * a = vector_new( 3, T_FLOAT ) ;
-    vector_t * b = vector_new( 3, T_FLOAT ) ;
+        InitCLEngine();
+    }
 
-    for (int i=0; i < a->len; i++){
+    void TearDown() {
+        ReleaseCLInfo(clinfo);
+    }
+
+    ~NeblinaCoreFixture() {
+        // cleanup any pending stuff, but no exceptions allowed
+    }
+
+};
+
+TEST_F(NeblinaCoreFixture, showDeviceList) {
+    showDevicesList();
+
+    EXPECT_EQ(1, 1);
+
+}
+
+TEST_F(NeblinaCoreFixture, addVectorF) {
+
+    int n = 3;
+    
+    vector_t * a = vector_new(n, T_FLOAT);
+    vector_t * b = vector_new(n, T_FLOAT);
+    vector_t * r = vector_new(n, T_FLOAT);
+
+    for (int i = 0; i < a->len; i++) {
         a->value.f[i] = 1.;
         b->value.f[i] = 1.;
     }
-    
-    vecreqdev( a ); vecreqdev( b ); 
-    int ii = 0;
-    object_t * out = (object_t *) malloc( sizeof( object_t ) );
-    vector_t * r = (vector_t *) malloc( sizeof( vector_t ) );
-    r->value.f = (double *) malloc( b->len * sizeof(double) );
 
-    r->mem = addVectorF( a->mem, b->mem, b->len ); 
+    vecreqdev(a);
+    vecreqdev(b);
+    vecreqdev(r);
 
+    r->mem = addVectorF(a->mem, b->mem, b->len);
 
-    r->len = b->len;
-    r->type = T_FLOAT;
-    r->location = LOCDEV;
-    type( *out ) = T_VECTOR;
-    vvalue( *out ) = (void *) r;
-    static void * ret[1];
-    ret[0] = (void *) out;
-    
-    int n = 3000;
-    double * out2 = (double *) malloc( n * sizeof(double) );
-    cl_int status = clEnqueueReadBuffer(clinfo.q, r->mem, CL_TRUE, 0, 3*sizeof(double), out2, 0, NULL, NULL);
+    double * out = (double *) malloc(n * sizeof (double));
+    status = clEnqueueReadBuffer(clinfo.q, r->mem, CL_TRUE, 0, n * sizeof (double), out, 0, NULL, NULL);
     CLERR
-    
-    for (int i=0; i < 3; ++i){
-        printf("res[%d]=%f\n",i,out2[i]);
+    EXPECT_EQ(0, status);
+
+    for (int i = 0; i < n; ++i) {
+        EXPECT_EQ(2., out[i]);
     }
 
-    ReleaseCLInfo(clinfo);
+}
 
+TEST_F(NeblinaCoreFixture, addVectorFC) {
+
+    int n = 3;
     
-    EXPECT_EQ(1, 1);
+    vector_t * a = vector_new(n, T_COMPLEX);
+    vector_t * b = vector_new(n, T_COMPLEX);
     
+    vector_t * r = vector_new(n, T_COMPLEX);
+    
+
+    for (int i = 0; i < a->len; i++) {
+        a->value.f[i] = 1.;
+        a->value.f[i+1] = 1.;
+        b->value.f[i] = 1.;
+        b->value.f[i+1] = 1.;
+    }
+
+    vecreqdev(a);
+    vecreqdev(b);
+    
+    vecreqdev(r);
+
+    r->mem = addVectorFC(a->mem, b->mem, b->len);
+   
+    double * out = (double *) malloc(n * COMPLEX_SIZE);
+    status = clEnqueueReadBuffer(clinfo.q, r->mem, CL_TRUE, 0, n * COMPLEX_SIZE, out, 0, NULL, NULL);
+    CLERR
+    EXPECT_EQ(0, status);
+
+    for (int i = 0; i < n; ++i) {
+        EXPECT_EQ(2., out[i]);
+        EXPECT_EQ(2., out[i+1]);
+    }
+
+}
+
+TEST_F(NeblinaCoreFixture, subVector) {
+
+    int n = 3;
+    
+    vector_t * a = vector_new(n, T_FLOAT);
+    vector_t * b = vector_new(n, T_FLOAT);
+    
+    vector_t * r = vector_new(n, T_FLOAT);
+    
+
+    for (int i = 0; i < a->len; i++) {
+        a->value.f[i] = 1.;
+        b->value.f[i] = 1.;
+    }
+
+    vecreqdev(a);
+    vecreqdev(b);
+    
+    vecreqdev(r);
+
+    r->mem = subVector(a->mem, b->mem, b->len);
+   
+    double * out = (double *) malloc(n * sizeof(double));
+    status = clEnqueueReadBuffer(clinfo.q, r->mem, CL_TRUE, 0, n * sizeof(double), out, 0, NULL, NULL);
+    CLERR
+    EXPECT_EQ(0, status);
+
+    for (int i = 0; i < n; ++i) {
+        EXPECT_EQ(0., out[i]);
+    }
+
 }
