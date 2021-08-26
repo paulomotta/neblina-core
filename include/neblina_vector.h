c857de1f3248610b2cd8b09752b48b0db03e3866
paulo <prmottajr@gmail.com>
2021-08-26 01:08:11 -0300

    Separated the vector creation functions on its own header Added the delete_vector function to correctly free memory

---
+++ include/neblina_vector.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   neblina_vector.h
+ * Author: paulo
+ *
+ * Created on 24 de agosto de 2021, 23:09
+ */
+
+#ifndef NEBLINA_VECTOR_H
+#define NEBLINA_VECTOR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "neblina.h"
+
+typedef struct __vector_t {
+    data_vector_u      value;
+    int                len;
+    data_type          type;
+    unsigned char      location;
+    cl_mem             mem;
+} vector_t;
+
+vector_t * vector_new( int len, data_type type ) ;
+void vector_delete( vector_t * v ) ;
+void vecreqhost( vector_t * v );
+void vecreqdev ( vector_t * v );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NEBLINA_VECTOR_H */
+
