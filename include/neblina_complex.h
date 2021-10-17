c36bed9dc10691632d0f8d898c56fe5bf0bbec2c
paulo <prmottajr@gmail.com>
2021-10-17 09:58:44 -0300

    Added new functions to create a complex scalar

---
+++ include/neblina_complex.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   neblina_complex.h
+ * Author: paulo
+ *
+ * Created on 16 de outubro de 2021, 11:30
+ */
+
+#ifndef NEBLINA_COMPLEX_H
+#define NEBLINA_COMPLEX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "neblina.h"
+
+typedef struct __complex_t {
+    double re;
+    double im;
+} complex_t;
+
+
+complex_t * complex_new( double real, double imaginary ) ;
+void complex_delete( complex_t * c ) ;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NEBLINA_COMPLEX_H */
+
---
+++ src/neblina_complex.c
@@ -0,0 +1,18 @@
+#include "libneblina.h"
+#include "neblina_complex.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+complex_t * complex_new( double real, double imaginary ) {
+    complex_t * ret = (complex_t *) malloc( sizeof( complex_t ) );
+
+    ret->re = real;
+    ret->im = imaginary;
+    
+    return ret;
+}
+
+void complex_delete( complex_t * c ) {
+    
+    free (c);
+}
