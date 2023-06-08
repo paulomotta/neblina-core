3992f3b79e2f29f168c53b7767cd66d97b9152c3
paulo <prmottajr@gmail.com>
2023-06-08 19:33:23 -0300

    test: update of unit tests to use the new structure based on bridge plugins

--- test/sparse_matrix_test.cpp
+++ test/sparse_matrix_test.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "libneblina.h"
 #include "clutils.h"
-#include "oclvector.h"
+#include "bridge_api.h"
 #include "neblina_std.h"
 //#include "neblina_vector.h"
 #include <iostream>
@@ -13,33 +13,22 @@ using namespace std;
 
 class SparseMatrixFixture : public ::testing::Test {
 protected:
-  cl_int status;
 public:
-
+    bridge_manager_t m;
+    int idx;
+    
     SparseMatrixFixture() {
-        // initialization code here
+        idx = 0;
+        load_plugin(&m, "/usr/local/lib64/libneblina-cpu-bridge.so", idx);
+        m.bridges[idx].InitEngine_f(0);
     }
 
     protected:
     static void SetUpTestSuite() {
         std::cerr << "Sparse Matrix TestSuiteSetup" << std::endl;
-        cl_int err;
-        cl_uint num_platforms;
-        
-        err = clGetPlatformIDs(0, NULL, &num_platforms);
-        if (err == CL_SUCCESS) {
-            //std::cout << "Success. Platforms available: " << num_platforms
-            //        << std::endl;
-        } else {
-            //std::cout << "Error. Platforms available: " << num_platforms
-            //        << std::endl;
-        }
-
-        InitCLEngine(0);
     }
 
     static void TearDownTestSuite() {
-        ReleaseCLInfo(clinfo);
     }
     
     void SetUp() {
@@ -63,37 +52,37 @@ TEST_F(SparseMatrixFixture, smatvec_multiply_WithSparseMatrixFloat) {
 
     int n = 10;
 
-    vector_t * a = vector_new(n, T_FLOAT);
-    smatrix_t * b = smatrix_new(n, n, T_FLOAT);
+    vector_t * a = m.bridges[idx].vector_new(n, T_FLOAT);
+    smatrix_t * b = m.bridges[idx].smatrix_new(n, n, T_FLOAT);
     vector_t * out;
 
     for (int i = 0; i < a->len; i++) {
         a->value.f[i] = 3.;
     }
 
-    smatrix_set_real_value(b, 0, 0, 3.);
-    smatrix_set_real_value(b, 0, 1, 3.);
-    smatrix_set_real_value(b, 0, 9, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 0, 0, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 0, 1, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 0, 9, 3.);
 
-    smatrix_set_real_value(b, 1, 1, 3.);
-    smatrix_set_real_value(b, 1, 5, 3.);
-    smatrix_set_real_value(b, 1, 8, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 1, 1, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 1, 5, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 1, 8, 3.);
 
-    smatrix_set_real_value(b, 2, 2, 3.);
-    smatrix_set_real_value(b, 2, 4, 3.);
-    smatrix_set_real_value(b, 2, 7, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 2, 2, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 2, 4, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 2, 7, 3.);
 
-    smatrix_set_real_value(b, 3, 3, 3.);
-    smatrix_set_real_value(b, 3, 1, 3.);
-    smatrix_set_real_value(b, 3, 6, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 3, 3, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 3, 1, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 3, 6, 3.);
 
-    smatrix_pack(b);
+    m.bridges[idx].smatrix_pack(b);
 
     //    for (int i=0; i < b->nrow * b->maxcols; i++) {
     //        printf("b->idx_col[%d]=%d b->[%d]=%lf \n",i,b->idx_col[i],i,b->m[i]);
     //    }
 
-    out = (vector_t *) smatvec_multiply(b, a);
+    out = (vector_t *) smatvec_multiply(&m, idx, b, a);
 
     EXPECT_EQ(27., out->value.f[0]);
     EXPECT_EQ(27., out->value.f[1]);
@@ -106,9 +95,9 @@ TEST_F(SparseMatrixFixture, smatvec_multiply_WithSparseMatrixFloat) {
     EXPECT_EQ(0., out->value.f[8]);
     EXPECT_EQ(0., out->value.f[9]);
     
-    vector_delete(a);
-    smatrix_delete(b);
-    vector_delete(out);
+    m.bridges[idx].vector_delete(a);
+    m.bridges[idx].smatrix_delete(b);
+    m.bridges[idx].vector_delete(out);
 
 }
 
@@ -116,37 +105,46 @@ TEST_F(SparseMatrixFixture, matvec_mul3_WithSparseMatrixFloat) {
 
     int n = 10;
 
-    vector_t * a = vector_new(n, T_FLOAT);
-    smatrix_t * b = smatrix_new(n, n, T_FLOAT);
+    vector_t * a = m.bridges[idx].vector_new(n, T_FLOAT);
+    smatrix_t * b = m.bridges[idx].smatrix_new(n, n, T_FLOAT);
     vector_t * r;
 
     for (int i = 0; i < a->len; i++) {
         a->value.f[i] = 3.;
     }
 
-    smatrix_set_real_value(b, 0, 0, 3.);
-    smatrix_set_real_value(b, 0, 1, 3.);
-    smatrix_set_real_value(b, 0, 9, 3.);
+    printf("aqui 1\n");
+    m.bridges[idx].smatrix_set_real_value(b, 0, 0, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 0, 1, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 0, 9, 3.);
 
-    smatrix_set_real_value(b, 1, 1, 3.);
-    smatrix_set_real_value(b, 1, 5, 3.);
-    smatrix_set_real_value(b, 1, 8, 3.);
+    printf("aqui 2\n");
+    m.bridges[idx].smatrix_set_real_value(b, 1, 1, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 1, 5, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 1, 8, 3.);
 
-    smatrix_set_real_value(b, 2, 2, 3.);
-    smatrix_set_real_value(b, 2, 4, 3.);
-    smatrix_set_real_value(b, 2, 7, 3.);
+    printf("aqui 3\n");
+    m.bridges[idx].smatrix_set_real_value(b, 2, 2, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 2, 4, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 2, 7, 3.);
 
-    smatrix_set_real_value(b, 3, 3, 3.);
-    smatrix_set_real_value(b, 3, 1, 3.);
-    smatrix_set_real_value(b, 3, 6, 3.);
+    printf("aqui 4\n");
+    m.bridges[idx].smatrix_set_real_value(b, 3, 3, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 3, 1, 3.);
+    m.bridges[idx].smatrix_set_real_value(b, 3, 6, 3.);
 
-    smatrix_pack(b);
+    printf("aqui 5\n");
+    m.bridges[idx].smatrix_pack(b);
+    printf("aqui 6\n");
 
     object_t ** in = convertToObject4(a, b);
+    printf("aqui 7\n");
 
-    r = (vector_t *) matvec_mul3((void **) in, NULL);
+    r = (vector_t *) matvec_mul3(&m, idx, (void **) in, NULL);
+    printf("aqui 8\n");
 
-    vecreqhost(r);
+    m.bridges[idx].vecreqhost(r);
+    printf("aqui 9\n");
 
     EXPECT_EQ(27., r->value.f[0]);
     EXPECT_EQ(27., r->value.f[1]);
@@ -159,9 +157,9 @@ TEST_F(SparseMatrixFixture, matvec_mul3_WithSparseMatrixFloat) {
     EXPECT_EQ(0., r->value.f[8]);
     EXPECT_EQ(0., r->value.f[9]);
     
-    vector_delete(a);
-    smatrix_delete(b);
-    vector_delete(r);
+    m.bridges[idx].vector_delete(a);
+    m.bridges[idx].smatrix_delete(b);
+    m.bridges[idx].vector_delete(r);
 
 }
 
@@ -169,8 +167,8 @@ TEST_F(SparseMatrixFixture, matvec_mul3_WithSparseMatrixComplex) {
 
     int n = 7000;
 
-    vector_t * a = vector_new(n, T_COMPLEX);
-    smatrix_t * b = smatrix_new(n, n, T_COMPLEX);
+    vector_t * a = m.bridges[idx].vector_new(n, T_COMPLEX);
+    smatrix_t * b = m.bridges[idx].smatrix_new(n, n, T_COMPLEX);
     vector_t * r;
 
     for (int i = 0; i < a->len; i++) {
@@ -178,33 +176,33 @@ TEST_F(SparseMatrixFixture, matvec_mul3_WithSparseMatrixComplex) {
         a->value.f[2 * i + 1] = 3.;
     }
 
-    smatrix_set_complex_value(b, 0, 0, 3., 3.);
-    smatrix_set_complex_value(b, 0, 1, 3., 3.);
-    smatrix_set_complex_value(b, 0, 9, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 0, 0, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 0, 1, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 0, 9, 3., 3.);
 
-    smatrix_set_complex_value(b, 1, 1, 3., 3.);
-    smatrix_set_complex_value(b, 1, 5, 3., 3.);
-    smatrix_set_complex_value(b, 1, 8, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 1, 1, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 1, 5, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 1, 8, 3., 3.);
 
-    smatrix_set_complex_value(b, 2, 2, 3., 3.);
-    smatrix_set_complex_value(b, 2, 4, 3., 3.);
-    smatrix_set_complex_value(b, 2, 7, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 2, 2, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 2, 4, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 2, 7, 3., 3.);
 
-    smatrix_set_complex_value(b, 3, 3, 3., 3.);
-    smatrix_set_complex_value(b, 3, 1, 3., 3.);
-    smatrix_set_complex_value(b, 3, 6, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 3, 3, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 3, 1, 3., 3.);
+    m.bridges[idx].smatrix_set_complex_value(b, 3, 6, 3., 3.);
     
     for (int i = 0; i < a->len; i++) {
-        smatrix_set_complex_value(b, 10, i, 3., 3.);
+        m.bridges[idx].smatrix_set_complex_value(b, 10, i, 3., 3.);
     }
 
-    smatrix_pack_complex(b);
+    m.bridges[idx].smatrix_pack_complex(b);
 
     object_t ** in = convertToObject4(a, b);
 
-    r = (vector_t *) matvec_mul3((void **) in, NULL);
+    r = (vector_t *) matvec_mul3(&m, idx, (void **) in, NULL);
 
-    vecreqhost(r);
+    m.bridges[idx].vecreqhost(r);
 
     EXPECT_EQ(0., r->value.f[0]);
     EXPECT_EQ(54., r->value.f[1]);
@@ -227,9 +225,9 @@ TEST_F(SparseMatrixFixture, matvec_mul3_WithSparseMatrixComplex) {
     EXPECT_EQ(0., r->value.f[18]);
     EXPECT_EQ(0., r->value.f[19]);
     
-    vector_delete(a);
-    smatrix_delete(b);
-    vector_delete(r);
+    m.bridges[idx].vector_delete(a);
+    m.bridges[idx].smatrix_delete(b);
+    m.bridges[idx].vector_delete(r);
 
 }
 
